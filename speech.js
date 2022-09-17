ffba796791d89e4581378b019d9047c4e118cb12
Elian-beep <elianoliver101@gmail.com>
2022-09-16 21:10:53 -0300

    html + css

---
+++ speech.js
@@ -0,0 +1,11 @@
+const textarea = document.querySelector('#textarea');
+const btnGravar = document.querySelector('#btnGravar');
+const btnParar = document.querySelector('#btnParar');
+const btnBaixar = document.querySelector('#btnBaixar');
+const btnLimpar = document.querySelector('#btnLimpar');
+
+class speechApi{
+    constructor(){
+        
+    }
+}
